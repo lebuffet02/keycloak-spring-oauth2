e381e85e3692f55b6a0f69c9f07a64886d67d33c
Lucas Buffet <lebuffet02@gmail.com>
2024-04-03 04:35:53 -0300

    mudando server no swagger

--- src/main/java/api/mykeycloak/config/SwaggerConfig.java
+++ src/main/java/api/mykeycloak/config/SwaggerConfig.java
@@ -1,10 +1,15 @@
 package api.mykeycloak.config;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.extensions.Extension;
+import io.swagger.v3.oas.annotations.extensions.ExtensionProperty;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.servers.ServerVariable;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,7 +23,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
                 email = "lebuffet02@gmail.com"
         ),
         license = @License(
-                name = "Apache 2.0")),
+                name = "Apache 2.0")), externalDocs = @ExternalDocumentation(url = "https://github.com/lebuffet02/keycloak-spring-oauth2"),
+        servers = @Server(url = "${server.port}", description = "porta alterada"),
         security = {
                 @SecurityRequirement(
                         name = "accessToken")
